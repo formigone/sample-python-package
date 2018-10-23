10777924a12b8255eec04364c7cc28ca421c836e
Rodrigo Silveira <rodrixar@gmail.com>
2018-10-23 12:22:02 -0600

    Greeter function

---
+++ formigone/demo.py
@@ -0,0 +1,6 @@
+def greet(name):
+    print('Hello, {}'.format(name))
+
+
+if __name__ == '__main__':
+    greet('Formigone')
---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name='Formigone',
+    version='v0.0.1',
+    packages=['formigone', ],
+)
